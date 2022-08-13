874ec5625982f6924ae2edaec900b313db1cf07d
Jalejandro2000 <jairo.angulo2017@uteq.edu.ec>
2022-08-13 15:52:44 -0500

    ok

---
+++ app/src/main/java/com/uteq/app/app_smart_pills_dispenser/CarerActivity.java
@@ -0,0 +1,19 @@
+package com.uteq.app.app_smart_pills_dispenser;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CarerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_carer);
+
+        TextView name
+    }
+}
