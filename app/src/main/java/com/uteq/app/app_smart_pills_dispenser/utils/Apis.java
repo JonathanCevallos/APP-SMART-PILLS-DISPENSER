c973b720512238cfe75332553e76e2346a71390d
Lin2808 <linjaviers@hotmail.com>
2022-08-13 19:09:36 -0500

    Corregido nombre de las clases

--- app/src/main/java/com/uteq/app/app_smart_pills_dispenser/utils/Apis.java
+++ app/src/main/java/com/uteq/app/app_smart_pills_dispenser/utils/Apis.java
@@ -1,12 +1,13 @@
 package com.uteq.app.app_smart_pills_dispenser.utils;
 
 import com.uteq.app.app_smart_pills_dispenser.services.DoctorService;
+import com.uteq.app.app_smart_pills_dispenser.services.DosageService;
 
 public class Apis {
     public static final String URL_001="https://app-smart-pills-dispenser.herokuapp.com/";
 
     public static DoctorService getDoctorService()
     {
-        return DoctorRetrofit.getDoctor(URL_001).create(DoctorService.class);
+        return ClientRetrofit.getClient(URL_001).create(DoctorService.class);
     }
 }
