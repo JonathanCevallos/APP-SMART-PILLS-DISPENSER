53d57f7b64457ad418612a0f0b1d6add0217a42f
Jalejandro2000 <jairo.angulo2017@uteq.edu.ec>
2022-08-14 10:54:34 -0500

    activity

--- app/src/main/java/com/uteq/app/app_smart_pills_dispenser/MainActivity.java
+++ app/src/main/java/com/uteq/app/app_smart_pills_dispenser/MainActivity.java
@@ -1,7 +1,11 @@
 package com.uteq.app.app_smart_pills_dispenser;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.uteq.app.app_smart_pills_dispenser.models.Dosage;
 import com.uteq.app.app_smart_pills_dispenser.services.DosageService;
@@ -12,12 +16,29 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRegister;
+
     DosageService dosageService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        btnRegister = findViewById(R.id.btnRegisterCarer);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CarerActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
     public void listDosage()
     {
--- app/src/main/java/com/uteq/app/app_smart_pills_dispenser/models/Dosage.java
+++ app/src/main/java/com/uteq/app/app_smart_pills_dispenser/models/Dosage.java
@@ -5,8 +5,7 @@ import java.util.Date;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+
 public class Dosage {
     private int id;
     private Date endDate;
@@ -17,4 +16,67 @@ public class Dosage {
     private int idMedicalTreatment;
     private int idPill;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public int getIdMedicalTreatment() {
+        return idMedicalTreatment;
+    }
+
+    public void setIdMedicalTreatment(int idMedicalTreatment) {
+        this.idMedicalTreatment = idMedicalTreatment;
+    }
+
+    public int getIdPill() {
+        return idPill;
+    }
+
+    public void setIdPill(int idPill) {
+        this.idPill = idPill;
+    }
 }
